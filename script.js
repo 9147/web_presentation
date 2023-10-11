2a268bb24c9af4227bfdc88af55ecd4ae38c24e0
manoj patil <manojpatil9147@gmail.com>
2023-10-11 16:25:02 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function openContactForm(){
+    console.log("openContactForm");
+    document.getElementById("contact").style.display = "block";
+}
+function closeContactForm(){
+    console.log("closeContactForm");
+    document.getElementById("contact").style.display = "none";
+}
+function change(){
+document.getElementById('img').src="background4.jpg"
+}
